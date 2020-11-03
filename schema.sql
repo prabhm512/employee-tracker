181128e093c10e8046db8a4906d734bbb4bf6f8f
Prabh Singh <prabhm512@gmail.com>
2020-11-03 17:10:57 +1100

    schema created

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeetracker_DB;
+CREATE DATABASE employeetracker_DB;
+
+USE employeetracker_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE `role` (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NULL,
+SALARY DECIMAL(50,2) NULL, 
+DEPARTMENT_ID INT,
+PRIMARY KEY (id)
+);
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NULL, 
+last_name VARCHAR(30) NULL,
+role_id INT,
+manager_id INT NULL,
+PRIMARY KEY (id)
+);
